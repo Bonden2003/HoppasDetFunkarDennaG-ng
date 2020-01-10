6bfa889b17b26b85114f45f1cb6a752feee37af8
Gabriel Bäck <59641868+Bonden2003@users.noreply.github.com>
2020-01-10 10:19:05 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HoppasDetFunkarDennaGång
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tjenare bonden här!");
+            Console.WriteLine("Tjenare bonden2003 här!");
             Console.WriteLine("Jag är en kille på 16 års ålder!");
             Console.WriteLine("Mina intressen är traktorer och snus!");
             Console.WriteLine("Jag liker snus!");
