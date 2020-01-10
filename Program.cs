c408c37dde5b1d41da2ed8e05163b1df05650129
Bonden2003 <59641868+Bonden2003@users.noreply.github.com>
2020-01-10 10:17:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HoppasDetFunkarDennaGång
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tjenare bonden här!");
+            Console.WriteLine("Jag är en kille på 16 års ålder!");
+            Console.WriteLine("Mina intressen är traktorer och snus!");
+            Console.WriteLine("Jag liker snus!");
+            Console.WriteLine("Speciellt en görgo ettan lös!");
+            Console.WriteLine("Det hade vart nått!");
+            Console.WriteLine("Vad jag gör på fritiden är hjälpa till hemma på bondgården och hänga ute med vänner!");
+            Console.WriteLine("Det var allt för mig #240-snusit!");
+        }
+    }
+}
