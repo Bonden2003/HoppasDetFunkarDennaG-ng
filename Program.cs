7d065c7517ff86d8ddb4574021ceb38311f772b6
Bonden2003 <59641868+Bonden2003@users.noreply.github.com>
2020-01-10 10:45:04 +0100

    Console.WriteLine("jag gillar bränslet diesel därför har jag skrivit en uppsatts på varför du välj diesel före andra bränslen");             Console.WriteLine("och jag har också skivit en debattartikel om varför man ska välja epa före mopedbilar");

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace HoppasDetFunkarDennaGång
             Console.WriteLine("Mina intressen är traktorer och snus!");
             Console.WriteLine("Jag liker snus!");
             Console.WriteLine("Speciellt en görgo ettan lös!");
+            Console.WriteLine("jag gillar bränslet diesel därför har jag skrivit en uppsatts på varför du välj diesel före andra bränslen");
+            Console.WriteLine("och jag har också skivit en debattartikel om varför man ska välja epa före mopedbilar");
             Console.WriteLine("Det hade vart nått!");
             Console.WriteLine("Vad jag gör på fritiden är hjälpa till hemma på bondgården och hänga ute med vänner!");
             Console.WriteLine("Det var allt för mig #240-snusit!");
